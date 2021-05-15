8231c8f931877c94cfebc4c8cffc9584d425787b
sonika09 <64416207+sonika09@users.noreply.github.com>
2021-05-15 13:45:46 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home';
+import Product from './components/pages/Product';
+import Services from './components/pages/Services';
+import Signup from './components/pages/Signup';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
   <Navbar/>
   <Switch>
     <Route path="/" exact component={Home}></Route>
+    <Route path="/services" component={Services}/>
+    <Route path="/products" component={Product}/>
+    <Route path="/sign-up" component={Signup}/>
   </Switch>
   </Router>
     </>
---
+++ src/components/pages/Signup.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../../App.css'
+function Signup() {
+    return <h1 className='sign-up'>SIGNUP</h1>
+}
+
+export default Signup
